891d11e170ed80d42b355d044aac1a9228833bdb
jcastro506 <castrojoshua805.com>
2020-11-01 20:09:34 -0500

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+class Anagram 
+
+    attr_accessor :word 
+
+    @@words = []
+
+    def initialize(word)
+        @word = word 
+        @@words << self 
+    end 
+
+
+    def match(words_array)
+        words_array.select {|word| word.split("").sort == @word.split("").sort}
+    end 
+
+
+end 
+
+
